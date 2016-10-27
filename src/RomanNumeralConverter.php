56e3d9e2b44b84820bb71d768e0da4f4d2a3fec4
Syed Huda <syed.huda@gmail.com>
2016-10-27 21:36:55 +0100

    Removed dummy test and added class for converter.

---
+++ src/RomanNumeralConverter.php
@@ -0,0 +1,9 @@
+<?php
+
+class RomanNumeralConverter
+{
+    public function convert()
+    {
+        return 'I';
+    }
+}
--- tests/RomanNumeralConverterTest.php
+++ tests/RomanNumeralConverterTest.php
@@ -3,8 +3,11 @@
 class RomanNumeralConverterTest extends PHPUnit_Framework_TestCase
 {
     /** @test */
-    public function it_returns_true()
+    public function it_calculates_the_roman_numeral_for_1()
     {
-        $this->assertTrue(true);
+        $converter = new RomanNumeralConverter();
+        $roman_numeral = $converter->convert(1);
+
+        $this->assertEquals('I', $roman_numeral);
     }
 }
