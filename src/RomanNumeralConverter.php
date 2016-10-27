ad7ab14e1924775a1559b218a996317990795118
Syed Huda <syed.huda@gmail.com>
2016-10-27 21:40:55 +0100

    Handle the number 5

--- src/RomanNumeralConverter.php
+++ src/RomanNumeralConverter.php
@@ -4,6 +4,9 @@ class RomanNumeralConverter
 {
     public function convert($number)
     {
+        if ($number == 5) {
+            return 'V';
+        }
         return str_repeat('I', $number);
     }
 }
--- tests/RomanNumeralConverterTest.php
+++ tests/RomanNumeralConverterTest.php
@@ -19,4 +19,13 @@ class RomanNumeralConverterTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals('II', $roman_numeral);
     }
+
+    /** @test */
+    public function it_calculates_the_roman_numeral_for_5()
+    {
+        $converter = new RomanNumeralConverter();
+        $roman_numeral = $converter->convert(5);
+
+        $this->assertEquals('V', $roman_numeral);
+    }
 }
